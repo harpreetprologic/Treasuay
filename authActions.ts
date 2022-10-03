cc3a2acc0beadf23c8ac9822bfa2ab5de61f30f3
harpreetprologic <111578256+harpreetprologic@users.noreply.github.com>
2022-10-03 17:44:36 +0530

    Add files via upload

---
+++ authActions.ts
@@ -0,0 +1,147 @@
+import {Alert} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {api} from '../../util';
+import {
+  SET_AUTH_LOADING,
+  SET_USER_DETAILS,
+  LOGOUT_USER,
+  GET_USER_PROFILE,
+  PUT_USER_PROFILE,
+  PUT_USER_PICTURE,
+} from '../types';
+import {KEY_USER} from '../../constants/keys';
+import {getErrorMessage} from '../../util/methods';
+
+export const login =
+  (username: string, password: string) => async (dispatch: any) => {
+    try {
+      dispatch({
+        type: SET_AUTH_LOADING,
+        payload: true,
+      });
+
+      const {data} = await api.post('auth/login/', {
+        username,
+        password,
+      });
+
+      dispatch({
+        type: SET_USER_DETAILS,
+        payload: {
+          userDetails: data,
+        },
+      });
+
+      AsyncStorage.setItem(KEY_USER, JSON.stringify(data));
+    } catch (error: any) {
+      console.log('...Error in login authActions', error);
+
+      dispatch({
+        type: SET_AUTH_LOADING,
+        payload: false,
+      });
+
+      Alert.alert(getErrorMessage(error));
+    }
+  };
+
+export const signup =
+  (name: string, treasury_name: string, email: string, password: string) =>
+  async (dispatch: any) => {
+    try {
+      dispatch({
+        type: SET_AUTH_LOADING,
+        payload: true,
+      });
+
+      const {data} = await api.post('auth/signup/', {
+        name,
+        treasury_name,
+        email,
+        password,
+      });
+
+      dispatch(login(email, password));
+
+      Alert.alert(data.detail);
+    } catch (error: any) {
+      console.log('...Error in sign up authActions', error);
+      Alert.alert(getErrorMessage(error));
+    } finally {
+      dispatch({
+        type: SET_AUTH_LOADING,
+        payload: false,
+      });
+    }
+  };
+
+export const getProfile = () => async (dispatch: any) => {
+  try {
+    dispatch({
+      type: SET_AUTH_LOADING,
+      payload: true,
+    });
+
+    const {data} = await api.get('auth/profile/');
+
+    dispatch({
+      type: SET_USER_DETAILS,
+      payload: {
+        userDetails: data.user,
+        followers: data.followers ?? 0,
+        following: data.following ?? 0,
+        treasures: data.treasures ?? 0,
+      },
+    });
+
+    AsyncStorage.setItem(KEY_USER, JSON.stringify(data.user));
+  } catch (error: any) {
+    console.log('...Error in profile authActions', error);
+    Alert.alert(getErrorMessage(error));
+  } finally {
+    dispatch({
+      type: GET_USER_PROFILE,
+      payload: false,
+    });
+  }
+};
+
+export const updateProfile =
+  (name: string, about: string) => async (dispatch: any) => {
+    try {
+      dispatch({
+        type: SET_AUTH_LOADING,
+        payload: true,
+      });
+
+      const {data} = await api.put('auth/profile/', {
+        name,
+        about,
+      });
+
+      dispatch({
+        type: SET_USER_DETAILS,
+        payload: {
+          userDetails: data,
+        },
+      });
+
+      Alert.alert('Profile updated successfully');
+    } catch (error: any) {
+      console.log('...Error in updateProfile authActions', error);
+      Alert.alert(getErrorMessage(error));
+    } finally {
+      dispatch({
+        type: SET_AUTH_LOADING,
+        payload: false,
+      });
+    }
+  };
+
+export const logout = () => (dispatch: any) => {
+  dispatch({
+    type: LOGOUT_USER,
+  });
+
+  AsyncStorage.removeItem('user');
+};
---
+++ authReducer.ts
@@ -0,0 +1,39 @@
+import { SET_AUTH_LOADING, SET_USER_DETAILS, LOGOUT_USER ,GET_USER_PROFILE} from "../types";
+
+const initialState = {
+    isLoading: false,
+    userDetails: null,
+    followers: 0,
+    following: 0,
+    treasures: 0
+}
+
+export default function(state=initialState, action: any) {
+    const {type, payload} = action;
+
+    switch(type) {
+        case SET_AUTH_LOADING:
+            return {
+                ...state,
+                isLoading: payload
+            }
+
+        case SET_USER_DETAILS:
+            return {
+                ...state,
+                userDetails: payload.userDetails,
+                followers: payload.followers ?? state.followers,
+                following: payload.following ?? state.following,
+                treasures: payload.treasures ?? state.treasures,
+                isLoading: false
+            }
+
+        case LOGOUT_USER:
+            return {
+                ...initialState
+            }
+        default:
+            return state;
+    }
+
+}
